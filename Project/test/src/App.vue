dd73dc3cf5cd2d7ca2cdc0faf68a57b18ee8bec6
duy-prog-2k <phamkhacduy.it@gmail.com>
2021-08-04 21:34:57 +0700

    push code Vue

---
+++ Project/test/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar>
+    </v-app-bar> -->
+    <v-main>
+      <v-banner single-line color="#04aa6d">
+        My Friend
+      </v-banner>
+      <friend-contact 
+      v-for="friend in friends" 
+      :key="friend.id" 
+      :name="friend.name"
+      :phone-number="friend.phone"
+
+      />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  name: "App",
+
+  components: {
+    FriendContact,
+  },
+
+  data(){
+    return{
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    }
+  }
+};
+</script>
---
+++ Project/test/src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card class="mx-auto" max-width="400" tile>
+    <v-card-title>{{ name }}</v-card-title>
+    <v-btn depressed class="ma-3" @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</v-btn>
+    <v-list v-if="detailsAreVisible">
+      <v-list-item>
+        {{ name }}
+      </v-list-item>
+      <v-list-item>
+        {{ phoneNumber }}
+      </v-list-item>
+      <v-list-item>
+        {{ emailAddress }}
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "friend-contact",
+  // props: [
+  //   'name', 
+  //   'phoneNumber',
+  //   'emailAddress'
+  // ],
+  // Props validation 
+  // props:{
+  //   name:{
+  //     type: String, 
+  //     require: true
+  //   },
+  //   phoneNumber:{
+  //     type:String,
+  //     require: true,
+  //   },
+  //   emailAddress:{
+  //     type: String, 
+  //     require: false,
+  //     default: 'Chua nhap email'
+  //   }
+  // },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods:{
+    toggleDetails: function(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
+
+<style></style>
